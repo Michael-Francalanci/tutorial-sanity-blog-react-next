9f632833e9b5c6e7bb280dad4e215d4c153e14f2
Michael Francalanci <michaelfrancalanci@Michaels-MacBook-Pro-2.local>
2021-02-19 21:48:23 +0100

    Next config

--- web/next.config.js
+++ web/next.config.js
@@ -1,20 +1,54 @@
+// const client = require('./client')
+
+// module.exports = {
+//   exportPathMap: async function (defaultPathMap) {
+//     const paths = await client
+//       .fetch('*[_type == "post" && defined(slug)].slug.current')
+//       .then(data =>
+//         data.reduce(
+//           (acc, slug) => ({
+//             '/': { page: '/' },
+//             ...acc,
+//             [`/post/${slug}`]: { page: '/post/[slug]', query: { slug } }
+//           }),
+//           defaultPathMap
+//         )
+//       )
+//       .catch(console.error)
+//     return paths
+//   }
+// }
+
+// next.config.js
+/*
+ * Let's reuse the client configuration. I have rewritten
+ * it as a CommonJS module just to make it runs in a Node
+ * environment without any fuzz.
+ */
+
 const client = require('./client')
 
 module.exports = {
+  // Make sure that your node enviroment supports async/await
   exportPathMap: async function (defaultPathMap) {
-    const paths = await client
-      .fetch('*[_type == "post" && defined(slug)].slug.current')
+  
+    const path = await client
+      // get all the posts and return those with slugs
+      .fetch('*[_type == "post"].slug.current')
       .then(data =>
+        // use reduce to build an object with routes
+        // and select the blog.js file, and send in the
+        // correct query paramater.
         data.reduce(
           (acc, slug) => ({
             '/': { page: '/' },
             ...acc,
-            [`/post/${slug}`]: { page: '/post/[slug]', query: { slug } }
+            [`/blog/${slug}`]: { page: '/blog', query: { slug } }
           }),
-          defaultPathMap
+          {}
         )
       )
       .catch(console.error)
-    return paths
+    return path
   }
 }
